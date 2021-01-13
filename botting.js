6806609a5a2985f0aa82c129df4532809daa9f19
KoxSosen <whyconfused2@gmail.com>
2021-01-13 11:39:51 +0100

    fix issues

--- botting.js
+++ botting.js
@@ -23,7 +23,6 @@ mineflayer.multiple(bots, ({ username }) => {
   const bot = mineflayer.createBot({
       host: 'localhost', // optional
       port: 25565,       // optional
-      username: 'email@example.com', // email and password are required only for
       password: '12345678',          // online-mode=true servers
      username 
     })
